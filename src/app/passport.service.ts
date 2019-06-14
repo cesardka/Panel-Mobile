283f4701022972bcc8c1d98741cab6ac81a4b1ef
César H <cesardka@hotmail.com>
2019-06-14 19:06:46 -0300

    Portar o que já havia sido produzido de jQuery -> Angular
    Começamos o processo de transição para o framework definitivo do projeto.
    Usaremos Angular para atualizar e modernizar o que será esse protótipo de Painel versão mobile
    
    Feito:
    - Login
    
    Telas pendentes:
    - Seleção de Cliente
    - Seleção de Produto
    - Página principal
    
    Contamos em componentezar e modularizar os menus, listagens e qualquer outra situação que possa ser aproveitado disso,
    para que no futuro possa ser feito apenas o encaixe de algo já feito ao invés de ter que adaptar, como é atualmente
    necessário fazer no Painel em PHP

---
+++ src/app/passport.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PassportService {
+
+	//public token: string;
+	private url = 'http://passport.alexandre.dev.com/auth';
+
+	constructor(private http: HttpClient) {
+		//const user = JSON.parse(localStorage.getItem('user'));
+		//this.token = user && user.token;
+	}
+
+	auth(email:string, password: string): Observable<any> {
+		return this.http.post<any>(this.url, {user: email, password: password, client_code: ''});
+		//.pipe(
+        	//map(data => {
+	          	// login bem-sucedido se houver um token jwt na resposta
+	          	//if (user && user.token) {
+	            	// armazenar detalhes do usuário e token jwt no localStorage para manter o usuário logado entre as atualizações da página
+	            	//localStorage.setItem('utoken', data.body['auth-token']);
+	          	//}
+
+	          	//return data;
+	        //})
+      	//);
+	}
+
+}
