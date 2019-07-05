fd5cb883aa7cda751003b174ee33ff563c66a4c7
César H <cesardka@hotmail.com>
2019-07-05 17:38:57 -0300

    Aplicação de route guards para redirecionamento de telas, para garantir que apenas usuarios logados possam navegar no sistema foi aplicado a ferramenta canActivate nas rotas, atualmente verificando a existência do token na localStorage, a utilização de chamada de api para autenticação a cada redirecionamento está sendo estudada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ClientesComponent } from './clientes/clientes.component';
 import { PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import { LoginGuard } from './login/login.guard';
 
 const routes: Routes = [
 	{ path: '', redirectTo: 'login', pathMatch: 'full'},
-	{ path: 'login', component: LoginComponent},
-	{ path: 'clientes', component: ClientesComponent},
-	{ path: '**', component: PageNotFoundComponent}
+	{ path: 'login', component: LoginComponent, canActivate: [LoginGuard]},
+	{ path: 'clientes', component: ClientesComponent, canActivate: [LoginGuard]},
+	{ path: '**', component: PageNotFoundComponent, canActivate: [LoginGuard]}
 ];
 
 @NgModule({
