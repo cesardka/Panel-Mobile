bb8151e9d0862d85372fecbc662481c69cf87007
César Hoffmann <cesardka@hotmail.com>
2019-08-02 18:22:10 -0300

    Listar clientes disponíveis para o usuário
    Ajustes ao componente item-cliente de listagem de contas que o usuário tem
    acesso.
    
    Removido o menu de navegação no topo antigo
    
    A intenção é começar a reformar este código para implementar melhor os
    conceitos de componentização do Angular. Principalmente a parte de
    serviços que ainda pecamos muito, que é preciso externalziar funções,
    chamadas para API, etc, ao invés de colocar dentro de componentes.

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemClienteComponent } from '../item-cliente/item-cliente.component';
+import { PassportService } from '../passport.service';
 
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
-  styleUrls: ['./clientes.component.css']
+  styleUrls: ['./clientes.component.css'],
+  providers: [ItemClienteComponent, PassportService],
 })
 export class ClientesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private passport: PassportService) { }
 
   ngOnInit() {
       
-    }
+  }
+
+  selectClient = (id: number) => {
+    console.log("get " + id + " = " + this.passport.Get(id));
+  }
+
+  staticListItems = () => {
+    return [
+      {
+        "id" : 1,
+        "title": "EmktPro",
+        "itemUrl": "",
+      },
+      {
+        "id" : 2,
+        "title": "Intranet",
+        "itemUrl": "https://panel.cesar.dev.com/img/logo/300000.svg",
+      },
+      {
+        "id" : 6,
+        "title": "",
+        "itemUrl": "",
+      },
+    ];
+  }
 }
